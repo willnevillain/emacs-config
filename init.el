227c863d9291cc779847dd552c0e692d659e46ab
Will Neville <william@neville.com>
2019-06-26 21:12:18 -0400

    up to date emacs config

--- init.el
+++ init.el
@@ -1,93 +1,42 @@
 ;; Emacs config
 
 ;; ------------------------------------
-;; Package Install
+;; Required package installation
 ;; ------------------------------------
 
+;; Add package repositories
 (require 'package)
-
-(add-to-list 'package-archives 
-	     '("melpa" . "http://melpa.org/packages/") t)
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-(defvar my-packages
-  '(better-defaults 
-    elpy ;; python
-    jedi
-    restclient ;; misc & themes
-    json-mode
-    dracula-theme))
+;; Basic TLS security checks
+(setq tls-checktrust t)
+(setq gnutls-verify-error t)
 
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-     my-packages)
+;; Ensure use-package and its deps installed
+(mapc
+ (lambda (package)
+   (if (not (package-installed-p package))
+       (progn
+         (package-refresh-contents)
+         (package-install package))))
+ '(use-package diminish bind-key))
 
-;; ------------------------------------
-;; Customization
-;; ------------------------------------
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
-;; Disable startup message
-(setq inhibit-startup-message t)
-
-;; Add themes to load path, load color theme
-(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
-(load-theme 'dracula t)
-
-;; Global line numbers
-(global-linum-mode t)
-
-;; Enable elpy
-(elpy-enable)
-
-;; Disable unnecessary GUI elemeents
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-
-;; Turn off bell sound, hate that mac 'boonk'
-(setq ring-bell-function 'ignore)
-
+(org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "bf390ecb203806cbe351b966a88fc3036f3ff68cd2547db6ee3676e87327b311" default)))
- '(fci-rule-color "#37474f")
- '(hl-sexp-background-color "#1c1f26")
- '(package-selected-packages (quote (dracula-theme nord-theme better-defaults)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#f36c60")
-     (40 . "#ff9800")
-     (60 . "#fff59d")
-     (80 . "#8bc34a")
-     (100 . "#81d4fa")
-     (120 . "#4dd0e1")
-     (140 . "#b39ddb")
-     (160 . "#f36c60")
-     (180 . "#ff9800")
-     (200 . "#fff59d")
-     (220 . "#8bc34a")
-     (240 . "#81d4fa")
-     (260 . "#4dd0e1")
-     (280 . "#b39ddb")
-     (300 . "#f36c60")
-     (320 . "#ff9800")
-     (340 . "#fff59d")
-     (360 . "#8bc34a"))))
- '(vc-annotate-very-old-color nil))
+    (gruvbox-theme dockerfile-mode yaml-mode use-package tagedit spacemacs-theme smex restclient rainbow-delimiters projectile org-plus-contrib neotree magit json-mode jedi ido-completing-read+ fish-mode fill-column-indicator exec-path-from-shell elpy elfeed doom-modeline diminish better-defaults auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
