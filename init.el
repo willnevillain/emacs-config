0929bcb354d5344da398f26bac1212a174906564
Will Neville <william@neville.com>
2019-07-07 11:38:54 -0400

    Swap ivy for helm, clean up init.el holdovers

--- init.el
+++ init.el
@@ -37,9 +37,13 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
- '(ivy-mode t)
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(package-selected-packages
    (quote
-    (company-lsp lsp-ui color-theme-sanityinc-tomorrow nyan-mode epresent org-download forge counsel helm org-bullets gruvbox-theme dockerfile-mode yaml-mode use-package tagedit spacemacs-theme smex restclient rainbow-delimiters projectile org-plus-contrib neotree magit json-mode jedi ido-completing-read+ fish-mode fill-column-indicator exec-path-from-shell elpy elfeed doom-modeline diminish better-defaults auto-compile)))
- '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#282828"))))
+    (exec-path-from-shell better-defaults neotree elfeed dockerfile-mode json-mode yaml-mode fish-mode restclient elpy projectile counsel helm-flx flx epresent org-download org-bullets forge magit all-the-icons nyan-mode gruvbox-theme color-theme-sanityinc-tomorrow auto-compile diminish use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block ((t (:inherit shadow :background "gray20")))))
